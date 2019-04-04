88061c2d81c209aa131144f52985111a2ad574aa
Ryan Stutchbury <ryan.stutch@gmail.com>
2019-04-04 03:50:20 +0000

    kinda

--- Frig.c
+++ Frig.c
@@ -1,12 +1,22 @@
-#include<std.io>
+#include<stdio.h>
 
 int main(){
-    char mix[]={a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
-    char in[500]=("text here");
+    char mix[]={"a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"};
+    char str[500]=("text here");
+    int i=0;
+    for(i=0; str[i] != '\0'; i++ ){
+        if (str[i]!=32){
+            if(str[i]==116){
+                str[i]=97;
+            }
+            if(str[i]==97){
+                str[i]=99;
+            }
+        }
+    }
     
     
     
-    
-    
+    printf("%s",str);
     return 0;
 }
