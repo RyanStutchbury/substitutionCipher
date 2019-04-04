a53b820352432efb90bd7f5aba98f69f2c98e2eb
Ryan Stutchbury <ryan.stutch@gmail.com>
2019-04-04 00:35:57 +0000

    yepp

--- Frig.c
+++ Frig.c
@@ -1,1 +1,12 @@
+#include<std.io>
 
+int main(){
+    char mix[]={a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
+    char in[500]=("text here");
+    
+    
+    
+    
+    
+    return 0;
+}
