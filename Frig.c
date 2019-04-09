cf0a9a189c16d82eba9cea8e61a2704a92cd13c5
Ryan Stutchbury <ryan.stutch@gmail.com>
2019-04-09 23:24:12 +0000

    Its ugly but it works

--- Frig.c
+++ Frig.c
@@ -1,19 +1,95 @@
 #include<stdio.h>
 
 int main(){
-    char mix[]={"a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"};
-    char str[500]=("text here");
+    char mix[]={" abcdefghijklmnopqrstuvwxyz"};
+    char key1[]={"qwertyuiopasdfghjklzxcvbnm"};
+    char str[500]=("lets hope this works");
     int i=0;
     for(i=0; str[i] != '\0'; i++ ){
-        if (str[i]!=32){
-            if(str[i]==116){
-                str[i]=97;
-            }
-            if(str[i]==97){
-                str[i]=99;
-            }
+        switch(str[i]){
+        case(32):
+            break;
+        case('a'):
+            str[i]='q';
+            break;  
+        case('b'):
+            str[i]='w';
+            break;
+        case('c'):
+            str[i]='e';
+            break;
+        case('d'):
+            str[i]='r';
+            break;
+        case('e'):
+            str[i]='t';
+            break;
+        case('f'):
+            str[i]='y';
+            break;
+        case('g'):
+            str[i]='u';
+            break;
+        case('h'):
+            str[i]='i';
+            break;
+        case('i'):
+            str[i]='o';
+            break;
+        case('j'):
+            str[i]='p';
+            break;
+        case('k'):
+            str[i]='a';
+            break;
+        case('l'):
+            str[i]='s';
+            break;
+        case('m'):
+            str[i]='d';
+            break;
+        case('n'):
+            str[i]='f';
+            break;
+        case('o'):
+            str[i]='g';
+            break;
+        case('p'):
+            str[i]='h';
+            break;
+        case('q'):
+            str[i]='j';
+            break;
+        case('r'):
+            str[i]='k';
+            break;  
+        case('s'):
+            str[i]='l';
+            break;
+        case('t'):
+            str[i]='z';
+            break;
+        case('u'):
+            str[i]='x';
+            break;
+        case('v'):
+            str[i]='c';
+            break;
+        case('w'):
+            str[i]='v';
+            break;
+        case('x'):
+            str[i]='b';
+            break;
+        case('y'):
+            str[i]='n';
+            break;
+        case('z'):
+            str[i]='m';
+            break;
         }
-    }
+
+            }
     
     
     
